66c66a6ee3b54d18f0279a49b1627994889a4852
Benyamgebru <benyamgebru26@gmail.com>
2022-12-16 02:34:31 +0300

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,8 +2,7 @@
 
 /**
  * main - prints the numbers from 1 to 100, followed by a new line
- * but for multiplies of three prints Fizz instead of the number
- * and for the multiples of five prints Buzz
+ * Description - but for multiplies of three prints Fizz instead of the number and for the multiples of five prints Buzz
  * Return: Always 0 (Success)
  */
 
