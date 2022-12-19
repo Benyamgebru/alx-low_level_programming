f0ed9731c015615e22e6fc4ef13914d2128c35e9
Benyamgebru <benyamgebru26@gmail.com>
2022-12-20 01:13:10 +0300

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,23 +9,16 @@
 
 void puts2(char *str)
 {
-	int longi = 0;
-	int t = 0;
-	char *y = str;
-	int o;
+	int i;
 
-	while (*y != '\0')
+	i = 0;
+	while (str[i] != '\0')
 	{
-		y++;
-		longi++;
-	}
-	t = longi - 1;
-	for (o = 0 ; o <= t ; o++)
-	{
-		if (o % 2 == 0)
+		if 9i % 2 == 0)
 		{
-			_putchar(str[0]);
+			_putchar(str[i]);
 		}
+		i++;
 	}
 	_putchar('\n');
 }
