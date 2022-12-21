d815ce2557940fd337cf391e3501b02f6de73819
Benyamgebru <benyamgebru26@gmail.com>
2022-12-21 20:34:40 +0300

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -14,8 +14,8 @@ char *string_toupper(char *n)
 	i = 0;
 	while (n[i] != '\0')
 	{
-		if (n[i] >= 'a' && n[i] <= 'Z')
-			n[i] = n[i] -32;
+		if (n[i] >= 'a' && n[i] <= 'zi')
+			n[i] = n[i] - 32;
 		i++;
 	}
 	return (n);
