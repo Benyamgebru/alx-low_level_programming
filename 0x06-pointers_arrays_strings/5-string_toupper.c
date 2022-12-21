aec9f457a20899af4b711d7c53febe68e280735d
Benyamgebru <benyamgebru26@gmail.com>
2022-12-21 20:33:33 +0300

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,7 +4,7 @@
  * string_toupper - change all lowercase to uppercase
  * @n: pointer
  *
- * Return: n
+ * Retrun: n
  */
 
 char *string_toupper(char *n)
@@ -14,7 +14,7 @@ char *string_toupper(char *n)
 	i = 0;
 	while (n[i] != '\0')
 	{
-		if (n[i] >= 'a' && n[i] <= 'z')
+		if (n[i] >= 'a' && n[i] <= 'Z')
 			n[i] = n[i] -32;
 		i++;
 	}
