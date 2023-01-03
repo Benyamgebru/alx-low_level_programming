528be57b0f4585817076764a93814aa654e744a0
Benyamgebru <benyamgebru26@gmail.com>
2023-01-03 19:08:59 +0300

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,10 +6,10 @@
  * @b: the desired value
  * @n: number of bytes to be changed
  *
- * Return: new string
+ * Return: changed array with new value for n bytes
  */
 
-char *_meset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	int i = 0;
 
