b335f4117232f5be8f628e3753e93fdb7bda4bb7
Benyamgebru <benyamgebru26@gmail.com>
2023-01-19 19:28:31 +0300

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,21 +6,23 @@
  * print_all - prints anything
  * @format: list of all arguments passed to the function
  *
- * Retrun: void
+ * Return: void
  */
 
 void print_all(const char * const format, ...)
 {
 	unsigned int i;
 	va_list args;
-	char *s, separator;
+	char *s, *separator;
 
 	va_start(args, format);
+
 	separator = "";
+
 	i = 0;
 	while (format && format[i])
 	{
-		swith (format[i])
+		switch (format[i])
 		{
 			case 'c':
 				printf("%s%c", separator, va_arg(args, int));
@@ -37,7 +39,7 @@ void print_all(const char * const format, ...)
 					s = "(nil)";
 				printf("%s%s", separator, s);
 				break;
-			default:
+			defualt:
 				i++;
 				continue;
 		}
